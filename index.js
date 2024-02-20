625c422cf909263fc58a21483f9da5620d6bdfcd
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-02-20 17:14:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.get('/api/test',(req,res)=>{
     console.log("connected");
 })
 
+//Create
+
 app.post('/api/project',async (req,res)=>{
     await mongoose.connect("mongodb+srv://ag8244932:Ac68xCCpwKJREF08@cluster0.81z9z64.mongodb.net/?retryWrites=true&w=majority");
     const {protit,prodesc,repolink,prolink}=req.body;
@@ -22,11 +24,51 @@ app.post('/api/project',async (req,res)=>{
     res.json(project);
 })
 
+//Read
+
 app.get('/api/projects',async (req,res)=>{
     await mongoose.connect("mongodb+srv://ag8244932:Ac68xCCpwKJREF08@cluster0.81z9z64.mongodb.net/?retryWrites=true&w=majority");
     const projects=await Project.find();
     res.json(projects);
 })
 
+//Delete
+
+
+app.post("/api/delete", async (req, res) => {
+    const { userid } = req.body;
+    try {
+        await mongoose.connect("mongodb+srv://ag8244932:Ac68xCCpwKJREF08@cluster0.81z9z64.mongodb.net/?retryWrites=true&w=majority");
+
+        const result = await Project.deleteOne({ _id: userid });
+        console.log(result);
+      res.send({ status: "Ok", data: "Deleted" });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+//update
+
+
+app.get('/api/update/:id',(req,res)=>{
+    mongoose.connect("mongodb+srv://ag8244932:Ac68xCCpwKJREF08@cluster0.81z9z64.mongodb.net/?retryWrites=true&w=majority");
+    const id=req.params.id;
+    Project.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err=> res.json(err))
+})
+
+app.put('/api/updateData/:id',(req,res)=>{
+    mongoose.connect("mongodb+srv://ag8244932:Ac68xCCpwKJREF08@cluster0.81z9z64.mongodb.net/?retryWrites=true&w=majority");
+    const id=req.params.id;
+    Project.findByIdAndUpdate({_id:id},{
+        protit:req.body.protit, 
+        prodesc : req.body.prodesc, 
+        repolink : req.body.repolink, 
+        prolink : req.body.prolink})
+    .then(users=>res.json(users))
+    .catch(err => res.json(err))
+})
 
 app.listen(5000);
